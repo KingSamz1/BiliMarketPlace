fd1e15a41055fd62256e0feb2b7eb0e5c491fe02
KingSamz1 <samzinovpro@gmail.com>
2026-04-29 03:47:38 +0300

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,1 +1,7 @@
+import streamlit as st
+from database import c
 
+st.title("Admin Panel")
+
+st.write("Users", c.execute("SELECT * FROM users").fetchall())
+st.write("Ads", c.execute("SELECT * FROM ads").fetchall())
