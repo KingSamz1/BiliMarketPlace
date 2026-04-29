a6ce5e4801ad60ba0a1cd5cd8c3a5557150af2bc
KingSamz1 <samzinovpro@gmail.com>
2026-04-29 03:44:02 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,15 @@
 
+from passlib.hash import pbkdf2_sha256
+from database import c, conn
+
+def register(name, phone, password):
+    hashed = pbkdf2_sha256.hash(password)
+    c.execute("INSERT INTO users(name, phone, password) VALUES(?,?,?)",
+              (name, phone, hashed))
+    conn.commit()
+
+def login(phone, password):
+    user = c.execute("SELECT * FROM users WHERE phone=?", (phone,)).fetchone()
+    if user and pbkdf2_sha256.verify(password, user[3]):
+        return user
+    return None
